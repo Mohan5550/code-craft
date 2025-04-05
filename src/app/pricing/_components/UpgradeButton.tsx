373192a5fa565065ea5f8b2e8fa6a8f2f09dd1b0
mohan <mohanramachandranp@gmail.com>
2025-04-05 11:46:53 +0530

    project completed

---
+++ src/app/pricing/_components/UpgradeButton.tsx
@@ -0,0 +1,19 @@
+import { Zap } from "lucide-react";
+import Link from "next/link";
+
+export default function UpgradeButton() {
+  const CHEKOUT_URL =
+    "https://codeeditor-craft.lemonsqueezy.com/buy/7dba0814-580c-42d6-8846-30999e4bda61";
+
+  return (
+    <Link
+      href={CHEKOUT_URL}
+      className="inline-flex items-center justify-center gap-2 px-8 py-4 text-white 
+        bg-gradient-to-r from-blue-500 to-blue-600 rounded-lg 
+        hover:from-blue-600 hover:to-blue-700 transition-all"
+    >
+      <Zap className="w-5 h-5" />
+      Upgrade to Pro
+    </Link>
+  );
+}
